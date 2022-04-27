40f7d67d3efa9a696d56a5a0c91f47c49e2b1368
Adamor Henner <adacandido9@gmail.com>
2022-04-27 14:43:33 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
